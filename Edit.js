28f3f3a9071135be3d06c8a5586d5fbfea7430f0
mariesusa <mari.remmler@hotmail.com>
2022-11-14 15:53:57 +0200

    Version 14.11.2022

--- Edit.js
+++ Edit.js
@@ -1,18 +1,100 @@
 import React from 'react';
+import { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Input } from 'react-native-elements';
+import { Button } from 'react-native-elements/dist/buttons/Button';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 import { getDatabase, push, ref, onValue, remove } from 'firebase/database';
 import database from './Firebase'
 
 export default function Edit ({ route, navigation }) {
     console.log( route );
-const { records } = route.params;
+//const { records } = route.params;
+
+const [formatOpen, setFormatOpen] = useState(false);
+const [formatValue, setFormatValue] = useState(null);
+const [format, setFormat] = useState([
+    { label: 'Vinyl', value: 'vinyl' },
+    { label: 'CD', value: 'cd' },
+    { label: 'Cassette', value: 'cassette' },
+]);
+
+const [conditionOpen, setConditionOpen] = useState(false);
+const [conditionValue, setConditionValue] = useState(null);
+const [condition, setCondition] = useState([
+    { label: 'Mint', value: 'mint' },
+    { label: 'Near Mint', value: 'nearMint' },
+    { label: 'Very Good Plus', value: 'veryGoodPlus' },
+    { label: 'Very Good', value: 'veryGood' },
+    { label: 'Good', value: 'good' },
+    { label: 'Fair', value: 'fair' },
+]);
 
 return (
     <View style={ styles.container }>
-        <Text>
-            Show things here { records }
-        </Text>
+        
+        <Text style={{marginTop: 10}}></Text>
+
+        <Input
+            label='Artist'
+        />
+
+        <Input
+            label='Album'
+        />
+
+        <Input
+            label='Year'
+        />
+
+        <DropDownPicker
+            open={ formatOpen }
+            value={ formatValue }
+                items={ format }
+                setOpen={ setFormatOpen }
+                setValue={ setFormatValue }
+                setItems={ setFormat }
+                placeholder='Select format'   
+        />
+        
+        <Text style={{marginTop: 10}}></Text>
+
+        <Input
+            label='Genre'
+        />
+
+        <DropDownPicker
+            open={ conditionOpen }
+            value={ conditionValue }
+                items={ condition }
+                setOpen={ setConditionOpen }
+                setValue={ setConditionValue }
+                setItems={ setCondition }
+                placeholder='Select condition'   
+        />
+
+        <Text style={{marginTop: 10}}></Text>
+        
+        <Input
+            label='Picture'
+        />
+
+        <Input
+            label='Info'
+        />
+
+        <Button
+            raised
+            titleStyle={{ fontWeight: '200' }}
+            buttonStyle={{
+                backgroundColor: '#000000',
+                borderRadius: 10,
+            }}
+            containerStyle={{ width: '60%' }}
+                icon={{ name: 'save', color: '#fff' }}
+                title='UPDATE'
+        />
     </View>
 );
 }
@@ -22,5 +104,5 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
-    }
+    },
 });
--- Home.js
+++ Home.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import ReactDOM from 'react';
+import { VictoryBar } from 'victory';
+
+const data = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000}
+];
 
 export default function Home() {
 
@@ -18,6 +27,29 @@ export default function Home() {
   );  
 }
 
+/*class Home extends React.Component {
+  render() {
+    return (
+      <View styles={ styles.container }>
+          <Text style={{ fontSize: 35 }}>
+            Music App
+          </Text>
+          <Text>
+            save your musics here
+          </Text>
+
+          <VictoryBar
+          data={ data }
+          // data accessor for x values
+          x="quarter"
+          // data accessor for y values
+          y="earnings"
+        />
+      </View>
+    )
+  }
+}*/
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -25,4 +57,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-;
+
+//ReactDOM.render(<Home />, mountNode);
+
+//export default Home;
+
